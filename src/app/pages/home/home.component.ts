2f2084d172c0f38ff60b7149bf0b66f2e9cc393f
Keerthana Vijaykumar <kevimach011997@gmail.com>
2023-12-08 22:19:08 -0700

    initial_commit

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { debounceTime } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+
+  productList: any [] = [];
+  constructor(private productService: ProductService){ // used for initialization
+
+  }
+
+  ngOnInit(): void { // used for api calls
+    debugger;
+    this.loadAllProducts();
+  }
+
+  loadAllProducts(){
+    debugger;
+    this.productService.getAllProducts().subscribe((result: any)=>{
+      this.productList = result.data;
+    })
+  }
+
+}
